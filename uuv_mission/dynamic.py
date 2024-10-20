240166b3e993a0ce71d28e7f769e2f0de3407598
Your NameFreddie <your.email@example.comfreddie@goodfellow.uk.n>
2024-10-20 17:47:08 +0100

    Controller implemented and notebook updated

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -102,7 +102,7 @@ class ClosedLoop:
         for t in range(T):
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
-            # Call your controller here
+            actions[t] = self.controller(observation_t, mission.reference[t])
             self.plant.transition(actions[t], disturbances[t])
 
         return Trajectory(positions)
