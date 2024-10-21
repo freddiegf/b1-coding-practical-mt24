f01bf3fce3874943c55e8c0bbb015ed3eb269c91
Your NameFreddie <your.email@example.comfreddie@goodfellow.uk.n>
2024-10-21 10:25:05 +0100

    Bug fixes

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,3 +1,4 @@
+
 class PDController:
     def __init__(self, kp, kd):
         """
@@ -10,17 +11,16 @@ class PDController:
         self.kd = kd
         self.previous_error = 0
 
-    def compute(self, setpoint, measured_value, dt):
+    def compute(self, setpoint, measured_value):
         """
         Compute the control signal based on the setpoint and measured value.
         
         :param setpoint: Desired target value
         :param measured_value: Current measured value
-        :param dt: Time interval since the last update
         :return: Control signal
         """
         error = setpoint - measured_value
-        derivative = (error - self.previous_error) / dt
+        derivative = (error - self.previous_error)
         output = self.kp * error + self.kd * derivative
         self.previous_error = error
         return output
